808a54f6425ee85f73b5460f2b1bac548bb483da
khanna.akash17@gmail.com <khanna.akash17@gmail.com>
2025-11-23 22:19:09 +0530

    Again commiting

--- src/main/java/utilities/ScreenshotUtil.java
+++ src/main/java/utilities/ScreenshotUtil.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+//Screenshotutil
 public class ScreenshotUtil {
      public static void captureScreenshot(WebDriver driver,String name) {
     	 try {
